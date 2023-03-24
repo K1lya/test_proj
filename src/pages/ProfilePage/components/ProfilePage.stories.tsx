bef27a2bc8a21cd12fd11cafd1252aef5ba5667b
Danila Kilishov <k1lyaa100@gmail.com>
2023-03-24 11:11:14 +0100

    add srtories to profile

---
+++ src/pages/ProfilePage/components/ProfilePage.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
+import { ThemeEnum } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/decorators/StoreDecorator';
+import ProfilePage from './ProfilePage';
+
+export default {
+  title: 'pages/ProfilePage',
+  component: ProfilePage,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof ProfilePage>;
+
+const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+Primary.decorators = [StoreDecorator({})];
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {};
+PrimaryDark.decorators = [ThemeDecorator(ThemeEnum.DARK), StoreDecorator({})];
